8e2e76ba0cbd4b009d5ccd3884584a8536ed03c6
Amine <a.sehlaoui96@gmail.com>
2023-11-25 01:22:58 -0800

    debug2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,6 @@
 #!/usr/bin/pup
-
 # Install a specific version of Flask (2.1.0)
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip',
-  require  => Package['python3-pip'], # Ensure that pip is installed first
+  provider => 'pip3',
 }
